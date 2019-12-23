f80608a1ad5703c3f01690f1124f93537cb34a84
Akhilesh <akhilmehta09@gmail.com>
2019-12-23 14:32:09 +0530

    Initial File loaded

---
+++ preTrafficHook.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+import os
+
+
+def lambda_handler(event, context):
+    deploymentId = event["DeploymentId"]
+    lifecycleEventHookExecutionId = event["LifecycleEventHookExecutionId"]
+    client = boto3.client("lambda", region_name="us-east-2")
+    codeclient = boto3.client("codedeploy")
+    payload = {"message": "Hi, you have been invoked."}
+    funname = os.environ["NewVersion"]
+    status = "Failed"
+    resp = client.invoke(
+        FunctionName=funname,
+        InvocationType="RequestResponse",
+        Payload=json.dumps(payload),
+    )
+
+    dic = json.loads(resp["Payload"].read().decode("utf-8"))
+
+    dic = json.loads(dic["body"])
+    # print(dic["status"]==200)
+
+    if dic["status"] == 200:
+        status = "Succeeded"
+    else:
+        status = "Failed"
+
+    response = codeclient.put_lifecycle_event_hook_execution_status(
+        deploymentId=deploymentId,
+        lifecycleEventHookExecutionId=lifecycleEventHookExecutionId,
+        status=status,
+    )
