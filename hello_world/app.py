b098d602b8ad42f4046a3535dac4c2ab85631478
Akhilesh <akhilmehta09@gmail.com>
2019-12-23 14:33:59 +0530

    Initial Load 2

---
+++ hello_world/app.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    response = s3.list_buckets()
+    buckets = [bucket["Name"] for bucket in response["Buckets"]]
+
+    # print("Bucket List: %s" % buckets)
+
+    # print("Status result ",context)
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"List": buckets, "event": event, "status": 300,}),
+    }
