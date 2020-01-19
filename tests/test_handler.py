fe11387e8e32b2c8274a06ad0d498b8adf39d3e7
AkhilMehta009 <akhilmehta09@gmail.com>
2020-01-20 01:08:39 +0530

    import bug fixed

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -64,7 +64,7 @@ def apigw_event():
     }
 
 
-def test_lambda_handler(apigw_event, self):
+def test_lambda_handler(apigw_event, mocker):
 
     ret = app.lambda_handler(apigw_event, "")
     data = json.loads(ret["body"])
